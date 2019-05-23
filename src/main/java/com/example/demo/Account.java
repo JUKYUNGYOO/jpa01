693e70458cea19b47ca42cdc662f10e7dbd6b5d4
jukyung <qhdwndb@gmail.com>
2019-05-23 17:40:08 +0900

    JPA

--- src/main/java/com/example/demo/Account.java
+++ src/main/java/com/example/demo/Account.java
@@ -1,9 +1,11 @@
 package com.example.demo;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 public class Account {
@@ -11,8 +13,43 @@ public class Account {
     @Id @GeneratedValue
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String username;
     private String password;
+
+    //Account(Owner)쪽에서 Study를 관리한다고 하면
+    //Account는 자신이 만든 Study 목록을 가지고 있다.
+
+    @OneToMany(mappedBy = "owner")
+    private Set<Study> studies = new HashSet<>();
+    //하나의 Owner은 여러개의 study를 만들 수 있다. 즉 Account가 관계를 가지고 있으므로, 주인 이다.
+
+
+    public Set<Study> getStudies() {
+        return studies;
+    }
+
+    public void setStudies(Set<Study> studies) {
+        this.studies = studies;
+    }
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date created = new Date();
+
+    private String yes;
+
+    @Transient
+    private String no;
+
+
+//Address 의 street을 컬럼을 home_street로 사용하겠다는 의미이다.
+    @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "street", column = @Column(name = "home_street"))
+    })
+    private Address address;
+
+
     private String email;
 
 
@@ -47,4 +84,16 @@ public class Account {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public void addStudy(Study study) {
+        this.getStudies().add(study); //optional
+        study.setOwner(this);	//반드시
+
+    }
+    public void removeStudy(Study study){
+        this.getStudies().remove(study);
+        study.setOwner(null); //참조하고 있던 study의 owner을 아니게
+
+    }
+
 }
--- src/main/java/com/example/demo/JpaRunner.java
+++ src/main/java/com/example/demo/JpaRunner.java
@@ -8,6 +8,11 @@ import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
 
 
 @Component
@@ -21,19 +26,64 @@ public class JpaRunner implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) throws Exception{
-        Account account = new Account();
-        account.setUsername("ann");
-        account.setPassword("hibernate");
+
+        List<Post> posts = entityManager
+                .createNativeQuery("SELECT * FROM Post", Post.class)
+                .getResultList();
+
+        posts.forEach(System.out::println);
+
+//        TypedQuery<Post> query = entityManager.createQuery("SELECT p FROM Post As p", Post.class);
+//        List<Post> posts = query.getResultList();
+//        posts.forEach(System.out::println);
 
 
-//        하이버네이트의 가장 핵심적인 API는 Session 이다.
+//        entityManager.createNamedQuery("all_post",Post.class);
 
-        Session session = entityManager.unwrap(Session.class);
-        session.save(account);
+        //타입 세이프한 방법
+//        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+//        CriteriaQuery<Post> query = builder.createQuery(Post.class);
+//        Root<Post> root = query.from(Post.class);
+//        query.select(root);
+//
+//        List<Post> posts = entityManager.createQuery(query).getResultList();
+//        posts.forEach(System.out::println);
 
-//        session으로 저장할 수 있다.
 
-        entityManager.persist(account);
+
+//        Post post  = new Post();
+//        post.setTitle("Spring Data Jpa는 언제보나");
+//
+//        Comment comment = new Comment();
+//        comment.setComment("빨리 보고 싶어요");
+//        post.addComment(comment);
+//
+//        Comment comment1 = new Comment();
+//        comment1.setComment("곧 보여드릴게요");
+//        post.addComment(comment1);
+//
+//
+//          Session session = entityManager.unwrap(Session.class);
+//          Post post = session.get(Post.class,4l);
+//          System.out.println("==========================");
+//          System.out.println(post.getTitle());
+//          post의 제목을 뿌리
+
+//        session.delete(post);
+
+//        session.save(post);
+//        post 만 테이블에 저장됨.
+//        Comment comment = session.get(Comment.class,5l);
+//        System.out.println("==============");
+//        System.out.println(comment.getComment());
+//        System.out.println(comment.getPost().getTitle());
+
+//        post.getComments().forEach(c -> {
+//            System.out.println("==================");
+//            System.out.println(c.getComment());
+//
+//        });
+//post에 관련된 comment를 전부 읽어옴.
 
     }
 
